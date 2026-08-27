56293779e3b232bf7540a77112d510cd704fc219
JAYA DILEEP <114493600+seethinajayadileep@users.noreply.github.com>
2026-08-27 21:23:39 +0530

    Add leetcode/single-element-in-a-sorted-array/solution.cpp from Leetcode-problems

---
+++ leetcode/single-element-in-a-sorted-array/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int low=0;
+        int high=nums.size()-1;
+        while(low<high){
+            int mid=(low+high)/2;
+            if(nums[mid]==nums[mid^1]){
+                low=mid+1;
+
+            }
+            else{
+                high=mid;
+            }
+        }
+        return nums[low];
+    }
+};
