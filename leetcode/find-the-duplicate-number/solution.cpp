8c1ce8495e720c9e3701849e7202ac82321fce8c
JAYA DILEEP <seethinajayadileep@hotmail.com>
2026-09-05 09:26:14 +0530

    Solve 287. Find the Duplicate Number on LeetCode

--- leetcode/find-the-duplicate-number/solution.cpp
+++ leetcode/find-the-duplicate-number/solution.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-       map<int,int> mpp;
-        for(int i=0;i<nums.size();i++){
-         mpp[nums[i]]++;
-         if(mpp[nums[i]]==2){
-            return nums[i];
-         }
-        }
-        return 0;
+      int slow=nums[0];
+        int fast=nums[0];
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(slow!=fast);
+        slow=nums[0];
+       while(slow!=fast){
+        slow=nums[slow];
+        fast=nums[fast];
+       }
+        return slow;
     }
 };
