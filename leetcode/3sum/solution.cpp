5945f44ab83b51e1d64ac3db7a50fd8bb9107ce0
JAYA DILEEP <seethinajayadileep@hotmail.com>
2026-09-06 14:19:47 +0530

    Solve 15. 3Sum on LeetCode

--- leetcode/3sum/solution.cpp
+++ leetcode/3sum/solution.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
         for(int i=0;i<nums.size();i++){
-           int j=i+1;
-           int k=nums.size()-1;
-           if(i>0 && nums[i]==nums[i-1]){
+            int j=i+1;
+            int k=nums.size()-1;
+            if(i>0 && nums[i]==nums[i-1]){
                 continue;
-               }
-           while(j<k){
-            int sum=nums[i]+nums[j]+nums[k];
-            if(sum<0){
-              j++;
             }
-            else if(sum>0){
-                k--;
-            }
-            else{
-                ans.push_back({nums[i],nums[j],nums[k]});
-                j++;
-                k--;
-                while(j<k&&nums[j]==nums[j-1] ){
-                    j++;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum<0){
+                 j++;
                 }
-                while(j<k && nums[k]==nums[k+1]){
+                else if(sum>0){
                     k--;
                 }
+                else{
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                    while(j<k&&nums[j]==nums[j-1]){
+                        j++;
+                    }
+                    while(j<k&&nums[k]==nums[k+1]){
+                        k--;
+                    }
+                }
+
             }
-               
-           }
-          
         }
-         return ans;
+        return ans;
     }
 };
