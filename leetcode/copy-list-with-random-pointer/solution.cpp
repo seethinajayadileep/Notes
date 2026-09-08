d7138c0bdbacd2761e8c02fb622a5f6bdfccf5ae
JAYA DILEEP <seethinajayadileep@hotmail.com>
2026-09-08 09:43:45 +0530

    Solve 138. Copy List with Random Pointer on LeetCode

---
+++ leetcode/copy-list-with-random-pointer/solution.cpp
@@ -0,0 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        unordered_map<Node*,Node*> mpp;
+        Node *temp=head;
+        while(temp){
+            Node *num=new Node(temp->val);
+            mpp[temp]=num;
+            temp=temp->next;
+        }
+        temp=head;
+        while(temp){
+           
+            mpp[temp]->next=mpp[temp->next];
+            mpp[temp]->random=mpp[temp->random];
+            temp=temp->next;
+        }
+        return mpp[head];
+        
+    }
+};
