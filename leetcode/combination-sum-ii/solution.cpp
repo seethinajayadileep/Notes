ad2621d373458c7a8d4b82310687f02e3e3eeb69
JAYA DILEEP <seethinajayadileep@hotmail.com>
2026-09-13 19:21:52 +0530

    Solve 40. Combination Sum II on LeetCode

--- leetcode/combination-sum-ii/solution.cpp
+++ leetcode/combination-sum-ii/solution.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    void fun(vector<int> &candidates,set<vector<int>> &ans,vector<int> &res,int index,int target){
+    void fun(vector<int> &candidates,vector<vector<int>> &ans,vector<int> &res,int index,int target){
         if(target==0){
-           ans.insert(res);
+           ans.push_back(res);
             return ;
         }
         if(target<0 || index==candidates.size()){
@@ -11,15 +11,18 @@ public:
         res.push_back(candidates[index]);
         fun(candidates,ans,res,index+1,target-candidates[index]);
         res.pop_back();
+        while(index+1<candidates.size()&&candidates[index]==candidates[index+1]){
+            index++;
+        }
        
         fun(candidates,ans,res,index+1,target);
 
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
         vector<int> res;
         sort(candidates.begin(),candidates.end());
         fun(candidates,ans,res,0,target);
-        return vector<vector<int>>(ans.begin(),ans.end());
+        return ans;
     }
 };
