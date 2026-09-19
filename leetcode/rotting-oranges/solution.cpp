29de30cb2218c7ae99c85cbfe49fa7bc12a3b498
JAYA DILEEP <seethinajayadileep@hotmail.com>
2026-09-19 20:33:15 +0530

    Solve 994. Rotting Oranges on LeetCode

--- leetcode/rotting-oranges/solution.cpp
+++ leetcode/rotting-oranges/solution.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         queue<pair<pair<int,int>,int>> q;
-        vector<vector<int>> visited(grid.size(),vector<int>(grid[0].size(),false));
+        vector<vector<int>> visited(grid.size(),vector<int>(grid[0].size(),0));
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]==2){
@@ -11,32 +11,33 @@ public:
                 }
             }
         }
+        int rows[]={0,0,-1,1};
+        int cols[]={-1,1,0,0};
         int maxtime=0;
         while(!q.empty()){
             auto ele=q.front();
-            int row=ele.first.first;
-            int col=ele.first.second;
+            int first=ele.first.first;
+            int second=ele.first.second;
             int time=ele.second;
             maxtime=max(time,maxtime);
             q.pop();
-            int rows[]={0,0,1,-1};
-            int cols[]={-1,1,0,0};
             for(int i=0;i<4;i++){
-                int newr=rows[i]+row;
-                int newc=cols[i]+col;
-                if(newr<0||newc<0||newr>=grid.size()||newc>=grid[0].size()||visited[newr][newc]==2||grid[newr][newc]==0){
-                    continue;
-                }
-                q.push({{newr,newc},time+1});
-                visited[newr][newc]=2;
-                grid[newr][newc]=2;
+                int newrow=rows[i]+first;
+                int newcol=cols[i]+second;
+                if(newrow<0||newcol<0|| newrow>=grid.size()|| newcol>=grid[0].size()||visited[newrow][newcol]==2||grid[newrow][newcol]==0){
+                   continue;
 
+                }
+                 q.push({{newrow,newcol},time+1});
+                 visited[newrow][newcol]=2;
 
             }
+          
+
         }
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==1){
+                if(grid[i][j]==1&&visited[i][j]!=2){
                     return -1;
                 }
             }
