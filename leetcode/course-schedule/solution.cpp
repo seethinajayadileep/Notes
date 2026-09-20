ed55f06bd0070319ad3acd3d6d834664abea3dfe
JAYA DILEEP <seethinajayadileep@hotmail.com>
2026-09-20 21:25:52 +0530

    Solve 207. Course Schedule on LeetCode

--- leetcode/course-schedule/solution.cpp
+++ leetcode/course-schedule/solution.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> indegree(numCourses,0);
         vector<int> adj[numCourses];
         for(int i=0;i<prerequisites.size();i++){
             int u=prerequisites[i][0];
@@ -8,28 +9,27 @@ public:
             adj[v].push_back(u);
 
         }
-        vector<int> indegree(numCourses,0);
         for(int i=0;i<numCourses;i++){
-            for(auto node:adj[i]){
-                indegree[node]++;
+            for(auto ele:adj[i]){
+                indegree[ele]++;
             }
-
         }
         queue<int> q;
-        for(int i=0;i<numCourses;i++){
+        for(int i=0;i<indegree.size();i++){
             if(indegree[i]==0){
                 q.push(i);
             }
         }
         while(!q.empty()){
-            int front=q.front();
+            int node=q.front();
             q.pop();
-            for(auto ele:adj[front]){
-                indegree[ele]--;
-                if(indegree[ele]==0){
-                    q.push(ele);
-                }
+            for(auto ele:adj[node]){
+               indegree[ele]--;
+            if(indegree[ele]==0){
+                q.push(ele);
             }
+            }
+            
         }
         for(int i=0;i<indegree.size();i++){
             if(indegree[i]!=0){
